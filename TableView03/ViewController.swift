52bf142c05b54ec40f1de72fea8580a26f1a3dcf
D7703_22 <d7703_22@promise.local>
2019-04-30 15:19:09 +0900

    commit 4/30

--- TableView03/ViewController.swift
+++ TableView03/ViewController.swift
@@ -8,11 +8,33 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController,UITableViewDataSource {
+    
+    @IBOutlet weak var myTableView: UITableView!
+    
+    var animals = ["Cat","Dog","Pig","Brid","Whale","Cow"]
+    var detallAnimals = ["고양이","개","돼지","새","고래","소"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        myTableView.dataSource = self
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return animals.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let myCell = myTableView.dequeueReusableCell(withIdentifier: "RE", for: indexPath)
+        let myRow = indexPath.row
+        
+        myCell.textLabel?.text = animals[myRow]
+        myCell.detailTextLabel?.text = detallAnimals[myRow]
+        
+        myCell.imageView?.image = UIImage(named: animals[myRow])
+        
+        return myCell
     }
 
 
